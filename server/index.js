f27455182edd5e2736a50fcb7d3a676ef384f2ce
callmeniyu <getniyashere@gmail.com>
2024-06-09 11:25:07 +0530

    set up authentication and session -Check /signup and /login route -develop local strategy -manage session

--- server/index.js
+++ server/index.js
@@ -5,18 +5,39 @@ import cors from "cors"
 import path from "path"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import brcypt from "bcrypt"
+import passport from "passport"
+import { Strategy as LocalStrategy } from "passport-local"
+import session from "express-session"
+import GoogleStrategy from "passport-google-oauth2"
 
 const port = 4000
+const saltRounds = 10;
 const app = express()
 dotenv.config()
 
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 mongoose.connect(process.env.MONGO_URL)
     .then(console.log("Connected to DB"))
     .catch((err) => console.log(err))
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
 
 const Book = mongoose.model("Book",{
     id: {
@@ -67,28 +88,32 @@ const User = mongoose.model("User", {
         type: String,
         require: true
     },
+    book: {
+        type:Object
+    }
 })
 
 app.get("/", (req, res) => {
     res.send("Working")
 })
-app.post("/addbook", async(req, res) => {
+app.post("/addbook", async (req, res) => {
     const newBook = new Book({
-        id:req.body.id,
-        name:req.body.name,
-        author:req.body.author,
-        isbn:req.body.isbn,
-        date:req.body.date,
-        rating:req.body.rating,
-        link:req.body.link,
-        summary:req.body.summary,
-        notes:req.body.notes,
-        img:req.body.img,
+        id: req.body.id,
+        name: req.body.name,
+        author: req.body.author,
+        isbn: req.body.isbn,
+        date: req.body.date,
+        rating: req.body.rating,
+        link: req.body.link,
+        summary: req.body.summary,
+        notes: req.body.notes,
+        img: req.body.img,
     })
     await newBook.save()
-    res.json({success:"true"})
+    res.json({ success: "true" })
     console.log("Saved new book")
-})
+});
+
 
 // MULTER STORAGE ENGINE
 const storage = multer.diskStorage({
@@ -107,6 +132,90 @@ app.post("/upload", upload.single("book"), (req, res) => {
         success: true,
         img_url: `http://localhost:${port}/images/${req.file.filename}`,
     })
+})
+
+app.post("/login", (req, res) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            return res.status(500).json({message:"Error occured in logging-in"})
+        } if (!user) {
+            return res.status(401).json({message:info.message})
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Error occured while authenticating" })
+            }
+            return res.status(200).json({ message: req.session })
+        });
+    })(req,res)
+})
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+    try {
+        const checkResult = await User.findOne({ email: email })
+        if (checkResult) {
+            res.status(400).json({ message: "User already registered, Please try login" })
+        } else {
+            brcypt.hash(password, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.log("Error hashing password:", err)
+                } else {
+                    const user = new User({
+                        username: username,
+                        email: email,
+                        password: hash
+                    })
+                    await user.save()
+                    console.log(`${username} saved to Users DB`)
+                    req.logIn(user, (err) => {
+                        if (err) {
+                            return res.status(500).json({ message: "Error occured in logging-in" })
+                        }
+                        return res.status(200).json({ session: req.session })
+                    })
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+passport.use("local",
+    new LocalStrategy({usernameField: 'email'},async function verify(email, password, cb) {
+        try {
+            const user = await User.findOne({ email: email })
+            if (user) {
+                const storedPassword = user.password
+                brcypt.compare(password, storedPassword, (err, valid)=> {
+                    if (err) {
+                        console.log("Error comparing passwords")
+                        return cb(err)
+                    } else {
+                        if (valid) {
+                            return cb(null,user)
+                        } else {
+                            return cb(null, false,{message:"Incorrect password"})
+                        }
+                    }
+                })
+            } else {
+                return cb(null, false)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+)
+
+passport.serializeUser((user, cb) => {
+    cb(null, user)
+})
+passport.deserializeUser((user, cb) => {
+    cb(null, user)
 })
 
 app.listen(port, () => {
